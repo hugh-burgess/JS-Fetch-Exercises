75fdaa7fed2f9db13da7be69a2498c08db1d5bb8
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-25 12:45:04 +0100

    3 done

---
+++ 3-catching-errors/solution.js
@@ -0,0 +1,15 @@
+fetch(`https://.typicode.com/todos/x`)
+  .then((response) => {
+    console.log(response);
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log(response.status);
+    }
+  })
+  .then((dataObject) => {
+    console.log(dataObject);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
